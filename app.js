755be4351b8c94fbea74d79fc0d86904df87ff86
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-07-15 12:54:29 -0400

    successfully make a https get fetch from weather api

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const https = require('https')
 
 const app = express()
 
 
 
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res){
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=bf683b1fc0347d23570cc1bafd3b2c6f"
+
+  https.get(url, function(data){
+    console.log(data);
+  })
+
   res.send("Serving is running and working correctly")
 })
 
