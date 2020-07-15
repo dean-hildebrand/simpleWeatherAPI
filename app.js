76a6c92709be4678c6209ef53dd7e80c7ef289ea
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-07-15 15:56:00 -0400

    installed body parser and created a form to dynamically change the location for the weather

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const https = require('https')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-
+app.use(bodyParser.urlencoded({extended: true}))
 
 
 app.get('/', function(req, res){
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=bf683b1fc0347d23570cc1bafd3b2c6f"
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', function(req, res){
+
+  const query = req.body.cityName
+  const apiKey = "bf683b1fc0347d23570cc1bafd3b2c6f"
+  const units = "metric"
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + units + "&appid=" + apiKey
 
   https.get(url, function(response){
     console.log(response.statusCode);
@@ -21,11 +31,12 @@ const url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metr
       const imageUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
       console.log(imageUrl);
       res.write("<p>The weather is currently " + weatherDescription + "<p>")
-      res.write("<h1>The temperature in London is " + temp + " degrees Celcius</h1>")
+      res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius</h1>")
       res.write("<img src=" + imageUrl + "/>")
       res.send()
     })
   })
+
 })
 
 
