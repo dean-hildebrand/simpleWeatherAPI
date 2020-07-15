75ebfc8760c8aefcd2a04a21b6dd9cd66a252f96
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-07-15 13:10:46 -0400

    finding differnet json for a response

--- app.js
+++ app.js
@@ -10,13 +10,19 @@ app.get('/', function(req, res){
 
 const url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=bf683b1fc0347d23570cc1bafd3b2c6f"
 
-  https.get(url, function(data){
-    console.log(data);
+  https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const description = weatherData.weather[0].description
+      console.log(description);
+    })
   })
 
   res.send("Serving is running and working correctly")
 })
-
 
 
 
